4930e93c773a1822b762d03a2ae23cb35880f4fb
ahmed7am1d <64076731+ahmed7am1d@users.noreply.github.com>
2022-01-03 20:00:35 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var myText =
+  "Hi it is Ahmed . Welcome to the Documentation of Testing The Functionality of Amazone Website. This Website was made to demonstraite the Test Suites and Test Cases and their details, for the Automation Testing Using Python and The Robotic FrameWork.",
+  i = 0;
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    var typeWriter = setInterval(function () {
+        document.getElementById('lead').textContent += myText[i];
+        i+=1;
+        if (i > myText.length - 1) {
+            clearInterval(typeWriter);
+        }
+    },30);
+
+    
+
+}, false);
+
+
+
